7e7244b1c57befcb602e13b0b70415bd5f03470c
watsonar <watson.arr@gmail.com>
2021-04-16 12:12:53 -0500

    Resetting things

--- src/data/process.py
+++ src/data/process.py
@@ -56,35 +56,3 @@ def calc_kcal_per_product(df):
     df.update(df_volume_vals)
     
     return df
-<<<<<<< HEAD
-
-"""
-def calc_grams_per_kcal(df):
-    df['kcal/kg'] = (df['kcal/kg'].fillna(df['kcal/unit'] 
-                     / df['item_vol_oz'] * 35.27396195))
-    df['g/kcal'] = 1000 / df['kcal/kg']
-    return df
-
-def calc_total_grams(df):
-    df['total_mass_lb'] = df['total_mass_lb'].fillna(df['attr_mass_lb'])
-    if df['total_mass_lb'].isna().any():
-        df['total_mass_lb'] = (df['total_mass_lb']
-                               .fillna(df['item_count'] 
-                               * df['item_vol_oz'] / 16))
-    df['total_mass_g'] = df['total_mass_lb'] * 453.59237
-    return df
-
-def calc_total_kcals(df):
-    df['total_kcals'] = df['kcal/kg'] * df['total_mass_g'] / 1000
-    return df
-
-def calc_usd_per_kcal(df):
-    df['usd/kcal'] = df['our_price'] / df['total_kcals']
-    return df
-
-def calc_usd_per_gram(df):
-    df['usd/g'] = df['our_price'] / df['total_mass_g']
-    return df
-"""
-=======
->>>>>>> master
